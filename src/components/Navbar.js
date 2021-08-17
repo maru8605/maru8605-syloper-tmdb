599b00fccd8960157d9809a2ba70c1ca9bfb59cd
maru8605 <maru8605@hotmail.com>
2021-08-16 22:11:06 -0300

    primeros cambios

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className='mt-5'>Hola desde nav </h2>
+        </div>
+    )
+}
+
+export default Navbar
