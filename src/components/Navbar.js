44f35cbd3c8747f013fab8fd21763a3652200cdd
maru8605 <maru8605@hotmail.com>
2021-08-17 00:59:10 -0300

    componente navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react';
+import '../sass/component/_navbar.scss'
+import Logo from '../image/logo-tm.svg'
 
 const Navbar = () => {
+
+    const [search, setSearch] = useState('')
+
+    const onChange = e => {
+        setSearch (    
+            [e.target.name] = e.target.value
+        )      
+        
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        console.log('haciendo submit')
+    }
+
     return (
-        <div>
-            <h2 className='mt-5'>Hola desde nav </h2>
-        </div>
+        <nav className='container-fluid py-4 nav d-flex justify-content-between'>
+            <div className='logo  mx-5'>
+                <img src={Logo} />
+            </div>
+            <form 
+            className='search mx-5' 
+            onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    placeholder='Buscar'
+                    className='search-input'
+                    onChange={onChange}
+                    name={search}
+                    
+                />
+                <input
+                    type='submit'
+                    value='Buscar'
+                    className='search-btn'
+                   
+                />
+            </form>
+        </nav>
     )
 }
 
